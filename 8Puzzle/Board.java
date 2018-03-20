2573535f20edb7e728605628a624fdfd42df10bd
yhtsao <konve123.am99@gmail.com>
2018-03-20 23:29:32 +0800

    Add basic structure for 8 puzzle problem

---
+++ 8Puzzle/Board.java
@@ -0,0 +1,84 @@
+public class Board {
+    /**
+     * construct a board from an n-by-n array of blocks
+     * (where blocks[i][j] = block in row i, column j)
+     *
+     * @param blocks
+     */
+    public Board(int[][] blocks) {
+
+    }
+
+    /**
+     * board dimension n
+     *
+     * @return
+     */
+    public int dimension() {
+        return 0;
+    }
+
+    /**
+     * number of blocks out of place
+     *
+     * @return
+     */
+    public int hamming() {
+        return 0;
+    }
+
+    /**
+     * sum of Manhattan distances between blocks and goal
+     *
+     * @return
+     */
+    public int manhattan() {
+        return 0;
+    }
+
+    /**
+     * is this board the goal board?
+     *
+     * @return
+     */
+    public boolean isGoal() {
+        return false;
+    }
+
+    /**
+     * a board that is obtained by exchanging any pair of blocks
+     *
+     * @return
+     */
+    public Board twin() {
+        return null;
+    }
+
+    /**
+     * does this board equal y?
+     *
+     * @param y
+     * @return
+     */
+    public boolean equals(Object y) {
+        return false;
+    }
+
+    /**
+     * all neighboring boards
+     *
+     * @return
+     */
+    public Iterable<Board> neighbors() {
+        return null;
+    }
+
+    /**
+     * string representation of this board (in the output format specified below)
+     *
+     * @return
+     */
+    public String toString() {
+        return null;
+    }
+}
---
+++ 8Puzzle/Solver.java
@@ -0,0 +1,38 @@
+public class Solver {
+
+    /**
+     * find a solution to the initial board (using the A* algorithm)
+     *
+     * @param initial
+     */
+    public Solver(Board initial) {
+
+    }
+
+    /**
+     * is the initial board solvable?
+     *
+     * @return
+     */
+    public boolean isSolvable() {
+        return false;
+    }
+
+    /**
+     * min number of moves to solve initial board; -1 if unsolvable
+     *
+     * @return
+     */
+    public int moves() {
+        return 0;
+    }
+
+    /**
+     * sequence of boards in a shortest solution; null if unsolvable
+     *
+     * @return
+     */
+    public Iterable<Board> solution() {
+        return null;
+    }
+}
