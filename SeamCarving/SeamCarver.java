86b8661f8d856bda31123c1185bc10184f883c7e
yhtsao <konve123.am99@gmail.com>
2018-04-25 08:10:40 +0800

    Initialize basic API of SeamCarver and testcases

---
+++ SeamCarving/SeamCarver.java
@@ -0,0 +1,86 @@
+import edu.princeton.cs.algs4.Picture;
+
+public class SeamCarver {
+    /**
+     * create a seam carver object based on the given picture
+     *
+     * @param picture
+     */
+    public SeamCarver(Picture picture) {
+
+    }
+
+    /**
+     * current picture
+     *
+     * @return
+     */
+    public Picture picture() {
+        return null;
+    }
+
+    /**
+     * width of current picture
+     *
+     * @return
+     */
+    public int width() {
+        return 0;
+    }
+
+    /**
+     * height of current picture
+     *
+     * @return
+     */
+    public int height() {
+        return 0;
+    }
+
+    /**
+     * energy of pixel at column x and row y
+     *
+     * @param x
+     * @param y
+     * @return
+     */
+    public double energy(int x, int y) {
+        return 0;
+    }
+
+    /**
+     * sequence of indices for horizontal seam
+     *
+     * @return
+     */
+    public int[] findHorizontalSeam() {
+        return null;
+    }
+
+    /**
+     * sequence of indices for vertical seam
+     *
+     * @return
+     */
+    public int[] findVerticalSeam() {
+        return null;
+    }
+
+    /**
+     * remove horizontal seam from current picture
+     *
+     * @param seam
+     */
+    public void removeHorizontalSeam(int[] seam) {
+
+    }
+
+    /**
+     * remove vertical seam from current picture
+     *
+     * @param seam
+     */
+    public void removeVerticalSeam(int[] seam) {
+
+    }
+}
