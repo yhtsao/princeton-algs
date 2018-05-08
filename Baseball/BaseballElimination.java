0f339568271940ededeaf1e1e21f268fb6bec5eb
yhtsao <konve123.am99@gmail.com>
2018-05-08 23:23:19 +0800

    Add basic API for baseball elimination

---
+++ Baseball/BaseballElimination.java
@@ -0,0 +1,89 @@
+public class BaseballElimination {
+    /**
+     * create a baseball division from given filename
+     *
+     * @param filename
+     */
+    public BaseballElimination(String filename) {
+
+    }
+
+    /**
+     * number of teams
+     *
+     * @return
+     */
+    public int numberOfTeams() {
+        return 0;
+    }
+
+    /**
+     * all teams
+     *
+     * @return
+     */
+    public Iterable<String> teams() {
+        return null;
+    }
+
+    /**
+     * number of wins for given team
+     *
+     * @param team
+     * @return
+     */
+    public int wins(String team) {
+        return 0;
+    }
+
+    /**
+     * number of losses for given team
+     *
+     * @param team
+     * @return
+     */
+    public int losses(String team) {
+        return 0;
+    }
+
+    /**
+     * number of remaining games for given team
+     *
+     * @param team
+     * @return
+     */
+    public int remaining(String team) {
+        return 0;
+    }
+
+    /**
+     * number of remaining games between team1 and team2
+     *
+     * @param team1
+     * @param team2
+     * @return
+     */
+    public int against(String team1, String team2) {
+        return 0;
+    }
+
+    /**
+     * is given team eliminated ?
+     *
+     * @param team
+     * @return
+     */
+    public boolean isEliminated(String team) {
+        return false;
+    }
+
+    /**
+     * subset R of teams that eliminates given team; null if not eliminated
+     *
+     * @param team
+     * @return
+     */
+    public Iterable<String> certificateOfElimination(String team) {
+        return null;
+    }
+}
